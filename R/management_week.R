d6c3e0a689a8f16ab7e8e9370efebc880be5b416
Ty Garber <tyler.garber@dfw.wa.gov>
2024-05-01 10:11:35 -0700

    stat week and management vecotrized functions from framrsquared

---
+++ R/management_week.R
@@ -0,0 +1,57 @@
+#' Vectorized approach to calculating the management week, returns
+#' an integer
+#' @param date A column with dates
+#' @export
+#' @examples
+#' \dontrun{
+#' data_frame |>
+#'   mutate(mngmt_week = management_week(date_field))
+#' }
+#'
+management_week <- function(date) {
+  UseMethod('management_week')
+}
+
+#' @export
+management_week.Date <- function(date) {
+  management_week_(date)
+}
+
+#' @export
+management_week.POSIXct <- function(date) {
+  # convert posixct to date
+  date <- as.Date(date)
+
+  management_week_(date)
+}
+
+#' @export
+management_week.character <- function(date) {
+  # try a few common formats
+  if(!anyNA(as.Date(date, '%Y-%m-%d'))) {
+
+    date <- as.Date(date, '%Y-%m-%d')
+
+  } else if(!anyNA(as.Date(date, '%m/%d/%Y'))) {
+
+    date <- as.Date(date, '%m/%d/%Y')
+
+  } else {
+
+    cli::cli_abort('Date is in an ambiguous format')
+
+  }
+
+  management_week_(date)
+}
+
+
+management_week_ <- function(date){
+  dplyr::if_else(
+    lubridate::wday(lubridate::floor_date(date, 'year')) == 1
+    ,
+    as.integer(strftime(date, '%U'))
+    ,
+    as.integer(strftime(date, '%U')) + 1
+  )
+}
---
+++ R/statistical_week.R
@@ -0,0 +1,61 @@
+#' Vectorized approach to calculating the statistical week, returns
+#' an integer
+#'
+#' Statistical weeks start on mondays, so the first statistical week of the year starts
+#' on the first monday of the year. (Contrast with management weeks which start on Sundays).
+#'
+#' @param date A vector of dates
+#' @export
+#' @examples
+#' \dontrun{
+#' data_frame |>
+#'   mutate(stat_week = statistical_week(date_field))
+#' }
+#'
+statistical_week <- function(date) {
+  UseMethod('statistical_week')
+}
+
+#' @export
+statistical_week.Date <- function(date) {
+  statistical_week_(date)
+}
+
+#' @export
+statistical_week.POSIXct <- function(date) {
+  # convert posixct to date
+  date <- as.Date(date)
+
+  statistical_week_(date)
+}
+
+#' @export
+statistical_week.character <- function(date) {
+  # try a few common formats
+  if(!anyNA(as.Date(date, '%Y-%m-%d'))) {
+
+    date <- as.Date(date, '%Y-%m-%d')
+
+  } else if(!anyNA(as.Date(date, '%m/%d/%Y'))) {
+
+    date <- as.Date(date, '%m/%d/%Y')
+
+  } else {
+
+    cli::cli_abort('Date is in an ambiguous format')
+
+  }
+
+  statistical_week_(date)
+}
+
+
+statistical_week_ <- function(date){
+  dplyr::if_else(
+    lubridate::wday(lubridate::floor_date(date, 'year')) == 2 ## If the first day of the year is a monday
+    ,
+    as.integer(strftime(date, '%W'))
+    ,
+    as.integer(strftime(date, '%W')) + 1
+  )
+}
